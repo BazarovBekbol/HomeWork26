ff8d3ef7bb45baed980a559ceec7c7887581322d
Bekbol B. <bazarovbekbol@gmail.com>
2024-04-30 15:48:02 +0600

    Add classes Movie and Director

---
+++ src/Movie.java
@@ -0,0 +1,17 @@
+public class Movie {
+    private String name;
+    private int year;
+    private Director director;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public Director getDirector() {
+        return director;
+    }
+}
